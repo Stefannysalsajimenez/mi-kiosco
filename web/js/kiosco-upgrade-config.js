01acaa098f99f1c6183ae22f432d35e590599ae3
Jhonn Pether <jpsalasjimenez@gmail.com>
2026-08-14 08:59:20 -0500

    chore: update kiosco configuration

--- web/js/kiosco-upgrade-config.js
+++ web/js/kiosco-upgrade-config.js
@@ -2,6 +2,7 @@
 
 window.KIOSCO_UPGRADE_CONFIG = Object.freeze({
   apiBaseUrl: '',
+  mediaApiBaseUrl: '',
   firebaseVapidKey: '',
   enableCallMeBot: false,
   storeUrl: 'https://mi-kiosco-c7313.web.app'
